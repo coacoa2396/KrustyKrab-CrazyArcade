7693754981d4f43d0764b8a03926014a69900961
PAKJUNGMIN <vhzptvhdhfl@gmail.com>
2024-05-13 10:02:43 +0900

    fix : 물폭탄 연쇄 반응 버그 수정

--- Assets/PAKJUNGMIN/01.Scripts/PlayerComponent/Bomb/WaterBombCollider.cs
+++ Assets/PAKJUNGMIN/01.Scripts/PlayerComponent/Bomb/WaterBombCollider.cs
@@ -6,14 +6,21 @@ using UnityEngine;
 
 public class WaterBombCollider : MonoBehaviour
 {
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        Debug.Log("asfdasdf");
+    private void OnTriggerEnter2D(Collider2D collision)
+    { //자식에 서로 콜라이더가 붙어있을 경우, 자식 콜라이더에 들어와도 부모 콜라이더에 들어온 것으로
+        //인식된다. 이를 방지하려면, 부모와 자식 서로에 RigidBody 컴포넌트가 필요하다.
         if (collision.gameObject.GetComponent<Drift>())
         {
-            Debug.Log("물폭탄 연쇄 작용");
-            GetComponentInParent<WaterBomb>().Explode();
+            try //나중에 정확한 로직을 써서 바꿀것. 예외 처리 지양. 0513 메모
+            {
+                GetComponentInParent<WaterBomb>().Explode();
+            }
+            catch
+            {
+                return;
+            }
+
         }
+
     }
 }
