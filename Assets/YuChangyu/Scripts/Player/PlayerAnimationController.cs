cf527c048b95b71cb16c24f4ac2aa4944b45b2a8
YuChangyu <kgadbcksrb@gmail.com>
2024-05-13 18:47:44 +0900

    feat : Idle 상태로 바꾸는 코드 구현

--- Assets/YuChangyu/Scripts/Player/PlayerAnimationController.cs
+++ Assets/YuChangyu/Scripts/Player/PlayerAnimationController.cs
@@ -8,8 +8,12 @@ using UnityEngine;
 /// </summary>
 public class PlayerAnimationController : MonoBehaviour
 {
+    [Header("Component")]
     [SerializeField] PlayerMediator player;
     [SerializeField] Animator animator;
+
+    [Header("variable")]
+    [SerializeField] Vector2 checkIdle;
 
     private void Start()
     {
@@ -19,18 +23,39 @@ public class PlayerAnimationController : MonoBehaviour
 
     private void LateUpdate()
     {
-        if (player.playerInputHandler.MoveDir == Vector3.zero)  // 조작이 없으면 리턴
+        if (player.playerInputHandler.MoveDir == Vector3.zero)  // 조작이 없으면 아이들 상태로 바꾸고 리턴
+        {
+            switch ((checkIdle.x, checkIdle.y))
+            {
+                case (1, 0):
+                    animator.SetTrigger("Right");
+                    break;
+                case (-1, 0):
+                    animator.SetTrigger("Left");
+                    break;
+                case (0, 1):
+                    animator.SetTrigger("Up");
+                    break;
+                case (0, -1):
+                    animator.SetTrigger("Down");
+                    break;
+            }
             return;
+        }
+
+        checkIdle = Vector2.zero;
 
         if (player.playerInputHandler.MoveDir.x == 0)           // 좌우 조작이 없는 경우
         {
             if (player.playerInputHandler.MoveDir.z < 0)        // 아랫 방향 조작인 경우
             {
                 animator.SetFloat("Y", -1);
+                checkIdle = new Vector2(0, -1);
             }
             else                                                // 윗 방향 조작인 경우
             {
                 animator.SetFloat("Y", 1);
+                checkIdle = new Vector2(0, 1);
             }
         }
         else if (player.playerInputHandler.MoveDir.z == 0)      // 상하 조작이 없는 경우
@@ -38,10 +63,12 @@ public class PlayerAnimationController : MonoBehaviour
             if (player.playerInputHandler.MoveDir.x < 0)        // 좌 방향 조작인 경우
             {
                 animator.SetFloat("X", -1);
+                checkIdle = new Vector2(-1, 0);
             }
             else                                                // 우 방향 조작인 경우
             {
                 animator.SetFloat("X", 1);
+                checkIdle = new Vector2(1, 0);
             }
         }
     }
