1125fa99c684db15b9fb101eb0a996930ac5891b
새롬 <non1233@naver.com>
2024-05-14 11:44:54 +0900

    feat : 방 입장 기능

--- Assets/KwonSaerom/Scripts/SubItem/UI_RoomToken.cs
+++ Assets/KwonSaerom/Scripts/SubItem/UI_RoomToken.cs
@@ -1,3 +1,5 @@
+using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,7 +8,6 @@ using UnityEngine.UI;
 
 public class UI_RoomToken : BaseUI
 {
-    private bool isActive = false;
     private RoomEntity roomInfo;
     enum GameObjects
     {
@@ -28,13 +29,12 @@ public class UI_RoomToken : BaseUI
     public void OffActive()
     {
         roomInfo = null;
-        isActive = false;
         gameObject.SetActive(false);
     }
 
     public void SetRoomInfo(RoomEntity room)
     {
-        isActive = true;
+        roomInfo = room;
         gameObject.SetActive(true);
         GetUI<TMP_Text>(GameObjects.RoomName.ToString()).text = room.RoomName;
         GetUI<TMP_Text>(GameObjects.RoomNumber.ToString()).text = room.RoomNum.ToString("D3");
@@ -43,6 +43,8 @@ public class UI_RoomToken : BaseUI
 
     public void EnterRoom()
     {
-
+        string key = $"{roomInfo.RoomNum}/{roomInfo.RoomName}";
+        LobbyManager.NowRoom = roomInfo;
+        PhotonNetwork.JoinRoom(key);
     }
 }
