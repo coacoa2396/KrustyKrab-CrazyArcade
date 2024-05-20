9564c55358012c56b047a72c9b226561a92f4f2c
YuChangyu <kgadbcksrb@gmail.com>
2024-05-20 14:23:41 +0900

    feat : 액티브아이템UI 구현

--- Assets/KwonSaerom/Scripts/Controller/PlayerPhotonContoller.cs
+++ Assets/KwonSaerom/Scripts/Controller/PlayerPhotonContoller.cs
@@ -1,18 +1,9 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerPhotonContoller : MonoBehaviour
+public class PlayerPhotonContoller : MonoBehaviourPun
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public PhotonView PV { get { return photonView; } }
 }
--- Assets/YuChangyu/Scripts/UI/ActiveItemUI.cs
+++ Assets/YuChangyu/Scripts/UI/ActiveItemUI.cs
@@ -15,51 +15,59 @@ public class ActiveItemUI : MonoBehaviourPun
 
     [SerializeField] Sprite[] sprites;
 
-    string name;
-
     private void Start()
     {
-        PlayerMediator[] players = FindObjectsOfType<PlayerMediator>();
-        foreach (PlayerMediator player in players)
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        PlayerPhotonContoller[] _players = new PlayerPhotonContoller[players.Length];
+        for (int i = 0; i < players.Length; i++)
         {
-            if (!photonView.IsMine)
+            _players[i] = players[i].GetComponent<PlayerPhotonContoller>();
+        }
+
+        foreach (PlayerPhotonContoller player in _players)
+        {
+            if (!player.PV.IsMine)
                 continue;
 
-            this.player = player;
+            this.player = player.GetComponent<PlayerMediator>();
             break;
         }
+
+        name = null;
     }
 
     private void LateUpdate()
     {
-
-        if (player.CurActiveItem.Name == null)
+        if (player.CurActiveItem == null)
         {
-            name = null;
+            itemImage.sprite = null;
+            itemImage.color = new Color(1, 1, 1, 0);
+            itemName.text = "";
+            itemUseNumber.text = "";
         }
-
-        switch (name)
+        else
         {
-            case "Needle":
-                itemImage.sprite = sprites[0];
-                itemName.text = "바늘";
-                itemUseNumber.text = $"X {player.CurActiveItem.UseNumber}";
-                break;
-            case "Dart":
-                itemImage.sprite = sprites[1];
-                itemName.text = "다트";
-                itemUseNumber.text = $"X {player.CurActiveItem.UseNumber}";
-                break;
-            case "Shield":
-                itemImage.sprite = sprites[2];
-                itemName.text = "방패";
-                itemUseNumber.text = $"X {player.CurActiveItem.UseNumber}";
-                break;
-            case null:
-                itemImage.sprite = null;
-                itemName.text = "";
-                itemUseNumber.text = "";
-                break;
+            switch (player.CurActiveItem.Name)
+            {
+                case "Needle":
+                    itemImage.sprite = sprites[0];
+                    itemImage.color = new Color(1, 1, 1, 1);
+                    itemName.text = "바늘";
+                    itemUseNumber.text = $"X {player.CurActiveItem.UseNumber}";
+                    break;
+                case "Dart":
+                    itemImage.sprite = sprites[1];
+                    itemImage.color = new Color(1, 1, 1, 1);
+                    itemName.text = "다트";
+                    itemUseNumber.text = $"X {player.CurActiveItem.UseNumber}";
+                    break;
+                case "Shield":
+                    itemImage.sprite = sprites[2];
+                    itemImage.color = new Color(1, 1, 1, 1);
+                    itemName.text = "방패";
+                    itemUseNumber.text = $"X {player.CurActiveItem.UseNumber}";
+                    break;
+            }
         }
     }
 }
