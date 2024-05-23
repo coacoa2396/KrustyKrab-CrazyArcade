536e631f17f60c2bad52e0e32b18cdd985162856
PAKJUNGMIN <vhzptvhdhfl@gmail.com>
2024-05-23 16:41:03 +0900

    feat : 아이템 데빌 기능 구현

--- Assets/YuChangyu/Scripts/Item/Devil/Devil.cs
+++ Assets/YuChangyu/Scripts/Item/Devil/Devil.cs
@@ -29,8 +29,10 @@ public class Devil : Item, IAcquirable
 
         Player = Player = collision.gameObject.GetComponent<PlayerMediator>();
 
-        // Player에서 PlayerState를 받아오고
+        Player.playerState.ChangeState(PlayerStateMachine.State.Devil);
 
+
+        // Player에서 PlayerState를 받아오고
         // Random.Range(0,2)를 해서
         // 0이면 반대입력상태
         // 1이면 물풍선 마구 놓는 상태
