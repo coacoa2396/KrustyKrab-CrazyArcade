7cc113fd2f936de94e93559461256c523fd81e0f
새롬 <non1233@naver.com>
2024-05-24 19:03:01 +0900

    fix : 물줄기 맞는 함수 마스터 클라이언트만 하게 수정

--- Assets/PAKJUNGMIN/01.Scripts/PlayerComponent/Dectector/PlayerStreamDectector.cs
+++ Assets/PAKJUNGMIN/01.Scripts/PlayerComponent/Dectector/PlayerStreamDectector.cs
@@ -1,4 +1,5 @@
 using pakjungmin;
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,7 @@ using UnityEngine;
 /// <summary>
 /// Class : 플레이어가 물줄기에 맞았는지 여부만 담당
 /// </summary>
-public class PlayerStreamDectector : MonoBehaviour
+public class PlayerStreamDectector : MonoBehaviourPun
 {
     PlayerMediator playerMediator;
 
@@ -19,9 +20,15 @@ public class PlayerStreamDectector : MonoBehaviour
     {
         if (collision.gameObject.GetComponent<Stream>())
         {
-            if(playerMediator.playerState.ownState == PlayerStateMachine.State.Trapped) { return; }
-           // Debug.Log("플레이어가 물줄기에 맞음");
-            playerMediator.playerState.ChangeState(PlayerStateMachine.State.Trapped);
+            photonView.RPC("CollisionStream", RpcTarget.MasterClient); // 권새롬추가 --> 마스터 클라이언트가 충돌함수를 호출함
         }
+    }
+
+    [PunRPC]
+    public void CollisionStream()
+    {
+        if (playerMediator.playerState.ownState == PlayerStateMachine.State.Trapped) { return; }
+        // Debug.Log("플레이어가 물줄기에 맞음");
+        playerMediator.playerState.ChangeState(PlayerStateMachine.State.Trapped);
     }
 }
