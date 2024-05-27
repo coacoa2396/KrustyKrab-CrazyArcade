3814a8b217b26260a8d57ee1cc41ce8d083ef384
YuChangyu <kgadbcksrb@gmail.com>
2024-05-27 09:10:22 +0900

    feat : 로비 및 룸의 사운드 세팅

--- Assets/YuChangyu/Scripts/Sound/ButtonSound.cs
+++ Assets/YuChangyu/Scripts/Sound/ButtonSound.cs
@@ -2,7 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-
+/// <summary>
+/// 제작 : 찬규 
+/// 버튼 클릭 사운드
+/// </summary>
 public class ButtonSound : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
 {
     public void OnPointerClick(PointerEventData eventData)
