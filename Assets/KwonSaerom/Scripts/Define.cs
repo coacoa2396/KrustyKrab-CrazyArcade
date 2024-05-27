2f7fa6eed399535c8a561523140a9e62d2477af6
새롬 <non1233@naver.com>
2024-05-27 16:54:33 +0900

    add : maxExp도 레벨에 따라 업데이트 추가

--- Assets/KwonSaerom/Scripts/Define.cs
+++ Assets/KwonSaerom/Scripts/Define.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Define
 {
-    public static float[] MAX_EXP = { 10, 50, 100};
+    public static float[] MAX_EXP = { 10, 50, 100,200,300,400,500,600};
     public const int CONNECT_NUM = 101;
     public const int DISCONNECT_NUM = 0;
     public enum Characters
